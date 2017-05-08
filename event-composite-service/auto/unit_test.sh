d649e82308197cc4378f973d1e0000b7e7c232b6
wyfan <yufan_wan@163.com>
2017-05-08 12:37:43 +0800

    update

--- event-composite-service/auto/unit_test.sh
+++ event-composite-service/auto/unit_test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# test in dood
-#docker run --rm -w /mnt -v /opt/jenkins_slave_home/workspace/composite-service/event-composite-service:/mnt:rw gradle/java
+# test into dood
 docker run --rm -w /mnt -v /opt/jenkins_slave_home/workspace/composite-service/event-composite-service:/mnt:rw gradle/java gradle clean test
